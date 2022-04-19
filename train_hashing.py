89b915513388b7cc844fffcd6719e714f0b4c7f1
ymcidence <ym_zmxncbv@hotmail.com>
2022-04-20 00:45:03 +0100

    Default Changelist

--- train_hashing.py
+++ train_hashing.py
@@ -39,6 +39,9 @@ def train_step(batch: dict, model: keras.Model, opt: keras.optimizers.Optimizer,
 
 
 def main():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
     conf = parser.parse_args()
     conf.cls_num = 10
     time_string = strftime("%a%d%b%Y-%H%M%S", gmtime())
